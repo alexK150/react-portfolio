5dc2c6ad5048c443c289579536776d8cf9b335cf
Alex <akuzin20@gmail.com>
2019-12-16 17:13:28 +0300

    v6 Footer styles

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import s from './Footer.module.css';
+import Link from './Link';
 
 const Footer = (props) => {
     return (
         <div className={s.footer}>
-            FOOTER
+            <div className={s.name}>
+                Alex
+                <div className={s.links}>
+                   <Link/>
+                   <Link/>
+                   <Link/>
+                </div>
+                <div className={s.rights}>
+                    2020 Все права защищены
+                </div>
+            </div>
         </div>
     )
 };
---
+++ src/components/footer/Link.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Footer.module.css'
+
+const Link = (props) => {
+    return <div className={s.icon}>
+
+    </div>
+};
+
+export default Link;
